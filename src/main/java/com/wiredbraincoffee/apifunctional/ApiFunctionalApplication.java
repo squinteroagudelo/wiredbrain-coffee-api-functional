d1d16428d994f519b032153144f3a0be646653de
Samuel Ricardo Quintero Agudelo <squinter@bancolombia.com.co>
2022-06-08 22:06:45 -0500

    Adding test

--- src/main/java/com/wiredbraincoffee/apifunctional/ApiFunctionalApplication.java
+++ src/main/java/com/wiredbraincoffee/apifunctional/ApiFunctionalApplication.java
@@ -1,5 +1,6 @@
 package com.wiredbraincoffee.apifunctional;
 
+import com.wiredbraincoffee.apifunctional.handler.ProductHandler;
 import com.wiredbraincoffee.apifunctional.model.Product;
 import com.wiredbraincoffee.apifunctional.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -7,28 +8,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @SpringBootApplication
 public class ApiFunctionalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiFunctionalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFunctionalApplication.class, args);
+    }
 
 
-	@Bean
-	CommandLineRunner init(ReactiveMongoOperations operations, ProductRepository repository){
-		return args -> {
-			Flux<Product> productFlux = Flux.just(
-							new Product(null, "Big Latte", 2.99),
-							new Product(null, "Big Decaf", 2.49),
-							new Product(null, "Green Tea", 1.99))
-					.flatMap(repository::save);
+    @Bean
+    CommandLineRunner init(ReactiveMongoOperations operations, ProductRepository repository) {
+        return args -> {
+            Flux<Product> productFlux = Flux.just(
+                            new Product(null, "Big Latte", 2.99),
+                            new Product(null, "Big Decaf", 2.49),
+                            new Product(null, "Green Tea", 1.99))
+                    .flatMap(repository::save);
 
-			productFlux
-					.thenMany(repository.findAll())
-					.subscribe(System.out::println);
+            productFlux
+                    .thenMany(repository.findAll())
+                    .subscribe(System.out::println);
 
 			/*operations.collectionExists(Product.class)
 					.flatMap(exists -> exists ? operations.dropCollection(Product.class) : Mono.just(exists))
@@ -36,7 +44,38 @@ public class ApiFunctionalApplication {
 					.thenMany(productFlux)
 					.thenMany(repository.findAll())
 					.subscribe(System.out::println);*/
-		};
-	}
+        };
+    }
 
+    @Bean
+    RouterFunction<ServerResponse> routes(ProductHandler handler) {
+        // Chained routes approach
+		/*return route()
+				.GET("/products/events", accept(TEXT_EVENT_STREAM), handler::getProductEvents)
+				.GET("/products/{id}", accept(APPLICATION_JSON), handler::getProduct)
+				.GET("/products", accept(APPLICATION_JSON), handler::getAllProducts)
+				.PUT("/products/{id}", accept(APPLICATION_JSON), handler::updateProduct)
+				.POST("/products", contentType(APPLICATION_JSON), handler::saveProduct)
+				.DELETE("/products/{id}", accept(APPLICATION_JSON), handler::deleteProduct)
+				.DELETE("/products", accept(APPLICATION_JSON), handler::deleteAllProducts)
+				.build();*/
+
+        // Nested routes approach
+        return route()
+                .path("/products",
+                        builder -> builder
+                                .nest(
+                                        accept(APPLICATION_JSON).or(contentType(APPLICATION_JSON))
+                                                .or(accept(TEXT_EVENT_STREAM)),
+                                        nestedBuilder -> nestedBuilder
+                                                .GET("/events", handler::getProductEvents)
+                                                .GET("/{id}", handler::getProduct)
+                                                .GET(handler::getAllProducts)
+                                                .PUT("/{id}", handler::updateProduct)
+                                                .POST(handler::saveProduct)
+                                )
+                                .DELETE("/{id}", handler::deleteProduct)
+                                .DELETE(handler::deleteAllProducts)
+                        ).build();
+    }
 }
---
+++ src/test/java/com/wiredbraincoffee/apifunctional/TestJUnit5BindToServer.java
@@ -0,0 +1,98 @@
+package com.wiredbraincoffee.apifunctional;
+
+import com.wiredbraincoffee.apifunctional.model.Product;
+import com.wiredbraincoffee.apifunctional.model.ProductEvent;
+import com.wiredbraincoffee.apifunctional.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestJUnit5BindToServer {
+    private WebTestClient client;
+    private List<Product> expectedList;
+
+    @Autowired
+    private ProductRepository repository;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.client = WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:" + port + "/products")
+                .build();
+
+        this.expectedList = repository.findAll().collectList().block();
+    }
+
+    @Test
+    public void testGetAllProducts() {
+        client
+                .get()
+                .uri("/")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Product.class)
+                .isEqualTo(expectedList);
+    }
+
+    @Test
+    public void testProductInvalidIdNotFound() {
+        client
+                .get()
+                .uri("/aaa")
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    @Test
+    public void testProductIdFound() {
+        Product expectedProduct = expectedList.get(0);
+        client
+                .get()
+                .uri("/{id}", expectedProduct.getId())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Product.class)
+                .isEqualTo(expectedProduct);
+    }
+
+    @Test
+    public void testProductEvents() {
+        FluxExchangeResult<ProductEvent> result = client
+                .get()
+                .uri("/events")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(ProductEvent.class);
+
+        ProductEvent expectedEvent = new ProductEvent(0L, "Product Event");
+
+        StepVerifier
+                .create(result.getResponseBody())
+                .expectNext(expectedEvent)
+                .expectNextCount(2)
+                .consumeNextWith(e -> assertEquals(Long.valueOf(3), e.getEventId()))
+                .thenCancel()
+                .verify();
+    }
+}
